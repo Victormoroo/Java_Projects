295ba8d0bc10d3f7787c7f80474eea1ec68449f3
Victormoroo <victorhugo0609@outlook.com>
2023-06-09 11:17:38 -0300

    feat: Practice with Lambda

--- Compass/lambda-practice/src/App.java
+++ Compass/lambda-practice/src/App.java
@@ -17,13 +17,89 @@ public class App {
         // Maneira atual de fazer o mesmo que esta no codigo acima
         walker( () -> System.out.println("Custom object walking... "));
 
-        ALambdaInterface aBlockOfCode = () -> {
+        Walkable aBlockOfCode = () -> {
             System.out.println("Custom object walking... ");
             System.out.println("the object tripped... ");
         };
+
+        walker(aBlockOfCode);
+
+        ALambdaInterface helloVar = () -> System.out.println("Hello there");
+        Calculate sumVar = (a, b) -> a + b;
+        helloVar.someMethod();
+        System.out.println(sumVar.compute(4, 6));
+
+        Calculate nonZeroDivider = (a, b) -> {
+            if(a == 0) {
+                return 0;
+            }
+            return a / b;
+        };
+        System.out.println(nonZeroDivider.compute(10, 2));
+
+        Char reverser = (s) -> {
+            String result = "";
+            for(int i = s.length()-1; i>=0; i--) {
+                result = result + s.charAt(i);
+            }
+            return result;
+        };
+        System.out.println(reverser.reverse("VICTOR"));
+
+        Number factorial = (n) -> {
+            int result = 1;
+            for(int i=1; i<=n; i++) {
+                result = i * result;
+            }
+            return result;
+        };
+        System.out.println(factorial.numberFactorial(10));
+    }
+
+    public  void sayHello() {
+        System.out.println("Hello there");
+    }
+
+    public int sum(int arg1, int arg2) {
+       return arg1 + arg2;
+    }
+
+    public int nonZeroDivider(int arg1, int arg2) {
+        if(arg1 == 0) {
+            return 0;
+        }
+        return arg1 / arg2;
+    }
+
+    public String reverse(String str) {
+        String result = "";
+        for(int i = str.length() -1; i>=0; i--) {
+            result = result + str.charAt(i);
+        }
+        return result;
+    }
+
+    public int factorial(int num) {
+        int result = 1;
+        for(int i=0; i<=num; i++) {
+            result = i * result;
+        }
+        return result;
     }
 
     public static void walker(Walkable walkableEntity) {
         walkableEntity.walk();
     }
+}
+
+interface Calculate {
+    public int compute(int a, int b);
+}
+
+interface Char {
+    public String reverse(String str);
+}
+
+interface Number {
+    public Integer numberFactorial(int num);
 }
--- Compass/lambda-practice/src/Walkable.java
+++ Compass/lambda-practice/src/Walkable.java
@@ -1,3 +1,5 @@
+@FunctionalInterface // se caso tentar adicionar outro metodo nessa interface, o programa ira reclamar
 public interface Walkable {
     public void walk();
+    //public void run();
 }
