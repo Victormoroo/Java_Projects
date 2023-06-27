3d803615b2cabe7245889d329c579ee8149bbf2a
Victormoroo <victorhugo0609@outlook.com>
2023-06-27 18:11:21 -0300

    feat: setter injection

---
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/02-spring-boot-spring-core/03-setter-injection/src/test/java/br/com/compass/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
