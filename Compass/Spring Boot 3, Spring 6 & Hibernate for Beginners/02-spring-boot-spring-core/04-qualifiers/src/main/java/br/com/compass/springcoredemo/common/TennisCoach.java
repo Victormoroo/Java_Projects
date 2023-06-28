c68b696c55a2c57642d917daa5ddbabda0f7e80f
Victormoroo <victorhugo0609@outlook.com>
2023-06-28 15:15:53 -0300

    feat: using @Qualifier

---
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/02-spring-boot-spring-core/04-qualifiers/src/main/java/br/com/compass/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,12 @@
+package br.com.compass.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+}
