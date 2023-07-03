a5bcccc05d1a86cf4511b282b0df71d98cd3dce6
Victormoroo <victorhugo0609@outlook.com>
2023-07-03 17:47:41 -0300

    feat: create a exception to BAD_REQUEST

--- Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/br/com/compass/demo/rest/StudentRestController.java
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/br/com/compass/demo/rest/StudentRestController.java
@@ -41,7 +41,7 @@ public class StudentRestController {
 
         // check the studentId again list size
         if((studentId >= theStudents.size()) || (studentId <= 0)) {
-            throw new StudentNotFoundException("Student id not found - " + studentId);
+            throw new StudentNotFoundException("Student id not found: " + studentId);
         }
 
         return theStudents.get(studentId-1); // sem o -1 ele busca pelo index, e colocando -1 fica como se fosse uma busca por id mesmo
@@ -60,5 +60,20 @@ public class StudentRestController {
 
         // return ResponseEntity
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler... to catch any exception (catch all)
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+
+        // create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
