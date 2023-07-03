60babe2472728eccec2cd4cb2cf41936f839bdf2
Victormoroo <victorhugo0609@outlook.com>
2023-07-03 16:45:55 -0300

    feat: added PostConstruct and search by id

--- Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/br/com/compass/demo/rest/StudentRestController.java
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/br/com/compass/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package br.com.compass.demo.rest;
 
 import br.com.compass.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +14,31 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    // define @PostConstruct to load the student data... only once!
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Victor", "Moro"));
         theStudents.add(new Student("Lucas", "Silva"));
         theStudents.add(new Student("James", "Gun"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return theStudents;
+    }
+
+    // define endpoint or "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list... keep it simple for now
+        return theStudents.get(studentId-1); // sem o -1 ele busca pelo index, e colocando -1 fica como se fosse uma busca por id mesmo
     }
 }
