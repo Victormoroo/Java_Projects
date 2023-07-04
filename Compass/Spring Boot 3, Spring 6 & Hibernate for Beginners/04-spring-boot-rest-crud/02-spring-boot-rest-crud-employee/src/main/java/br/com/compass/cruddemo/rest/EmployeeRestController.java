c64827ffffd9bbc449708afa0300ee265f1c548a
Victormoroo <victorhugo0609@outlook.com>
2023-07-04 17:54:50 -0300

    feat: crested method Post

--- Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/br/com/compass/cruddemo/rest/EmployeeRestController.java
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/br/com/compass/cruddemo/rest/EmployeeRestController.java
@@ -3,10 +3,7 @@ package br.com.compass.cruddemo.rest;
 import br.com.compass.cruddemo.entity.Employee;
 import br.com.compass.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,5 +34,19 @@ public class EmployeeRestController {
             throw new RuntimeException("Employee id not found: " + employeeId);
         }
         return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+
+        // also just in case they pass an in JSON... set id to 0
+        // this is to force a save of new item... instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
     }
 }
