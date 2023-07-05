4f7f6f16a309fe53cda4d86ef17925373bbcf59d
Victormoroo <victorhugo0609@outlook.com>
2023-07-05 15:14:49 -0300

    feat: created a new project

---
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/br/com/compass/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package br.com.compass.cruddemo.dao;
+
+import br.com.compass.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
---
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/br/com/compass/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package br.com.compass.cruddemo.service;
+
+import br.com.compass.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
