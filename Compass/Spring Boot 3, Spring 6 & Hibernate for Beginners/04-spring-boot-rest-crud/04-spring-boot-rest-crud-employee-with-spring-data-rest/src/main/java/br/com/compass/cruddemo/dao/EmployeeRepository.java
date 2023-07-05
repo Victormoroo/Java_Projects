f456cce3f2806bc9a4acb696ebe566c79bcceb38
Victormoroo <victorhugo0609@outlook.com>
2023-07-05 16:23:55 -0300

    feat: changing amount of data delivered in JSON

--- Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/br/com/compass/cruddemo/dao/EmployeeRepository.java
+++ Compass/Spring Boot 3, Spring 6 & Hibernate for Beginners/04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/br/com/compass/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package br.com.compass.cruddemo.dao;
 
 import br.com.compass.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it... no need to write any code LOL!
