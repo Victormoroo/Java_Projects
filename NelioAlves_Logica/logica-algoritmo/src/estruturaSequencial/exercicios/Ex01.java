1cf4b39d831ca01d04d58b7b3b2f741050c7810e
Victormoroo <victorhugo0609@outlook.com>
2023-07-06 11:07:17 -0300

    feat: resolucao ex01 do pdf

---
+++ NelioAlves_Logica/logica-algoritmo/src/estruturaSequencial/exercicios/Ex01.java
@@ -0,0 +1,22 @@
+package estruturaSequencial.exercicios;
+
+import java.io.PrintStream;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double valorM = sc.nextDouble();
+
+        double area = largura * comprimento;
+
+        System.out.printf("ÁREA = %.2f%n", area);
+        System.out.printf("PREÇO = %.2f", valorM * area);
+    }
+}
